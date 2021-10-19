9161d442b354388290275df26d3e85bae9b82ffc
Thomas Viana <thomasviana16@gmail.com>
2021-10-19 17:39:59 -0500

    Import and implement Dice 3D Model

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -18,27 +18,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
-        
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-        
-        let material = SCNMaterial()
-        
-        material.diffuse.contents = UIColor.red
-        
-        cube.materials = [material]
-        
-        let node = SCNNode()
-        
-        node.position = SCNVector3(0, 0.1, -0.5)
-        
-        node.geometry = cube
-        
-        sceneView.scene.rootNode.addChildNode(node)
+//
+//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//
+//        let material = SCNMaterial()
+//
+//        material.diffuse.contents = UIColor.red
+//
+//        cube.materials = [material]
+//
+//        let node = SCNNode()
+//
+//        node.position = SCNVector3(0, 0.1, -0.5)
+//
+//        node.geometry = cube
+//
+//        sceneView.scene.rootNode.addChildNode(node)
         
         sceneView.autoenablesDefaultLighting = true
         
 //        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            diceNode.position = SCNVector3(0, 0, -0.1)
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
+        
+      
 //
 //        // Set the scene to the view
 //        sceneView.scene = scene
