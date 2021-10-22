3bbf7c1afd92f80d6f3506467b74fe02ce18007a
Thomas Viana <thomasviana16@gmail.com>
2021-10-22 13:38:30 -0500

    Roll all dices with motion

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -10,6 +10,8 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    var diceArray = [SCNNode]()
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -80,23 +82,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                         y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
                         z: hitResult.worldTransform.columns.3.z
                     )
+                    
+                    diceArray.append(diceNode)
                 
                     sceneView.scene.rootNode.addChildNode(diceNode)
                     
-                    let rendomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
-                    let rendomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
-                    
-                    diceNode.runAction(SCNAction.rotateBy(
-                        x: CGFloat(rendomX * 5),
-                        y: 0,
-                        z: CGFloat(rendomZ * 5),
-                        duration: 0.5)
-                    )
+                    roll(dice: diceNode)
 
                 }
                 
             }
         }
+    }
+    
+    func rollAll() {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice: dice)
+            }
+        }
+    }
+    
+    func roll(dice: SCNNode) {
+        
+        let rendomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        let rendomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        dice.runAction(SCNAction.rotateBy(
+            x: CGFloat(rendomX * 5),
+            y: 0,
+            z: CGFloat(rendomZ * 5),
+            duration: 0.5)
+        )
+    }
+    
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollAll()
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
